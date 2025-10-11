cf4e9be5cd3a9f514e94214b9e6be6ce8809e2a8
seggebai <felix@seggebaeing.de>
2025-10-11 19:08:46 +0200

    Initial commit

---
+++ src/sw/App.java
@@ -0,0 +1,11 @@
+package sw;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO
+    }
+}
---
+++ src/sw/Main.java
@@ -0,0 +1,9 @@
+package sw;
+
+import javafx.application.Application;
+
+public class Main {
+    static void main() {
+        Application.launch(App.class);
+    }
+}

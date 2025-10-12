2a511f9350684727f49f34d3cc78f83d8e140636
seggebai <felix@seggebaeing.de>
2025-10-12 15:31:30 +0200

    feat: Add splash screen

--- src/sw/App.java
+++ src/sw/App.java
@@ -1,11 +1,27 @@
 package sw;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.Objects;
 
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        // TODO
+    public void start(Stage primaryStage) {
+        // For now just show the splash screen.
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Image splashImg = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/splash.png")));
+        ImageView imgView = new ImageView(splashImg);
+        imgView.setPreserveRatio(true);
+        imgView.setFitWidth(800);
+        StackPane splashRoot = new StackPane(imgView);
+        Scene splashScene = new Scene(splashRoot);
+        primaryStage.setScene(splashScene);
+        primaryStage.show();
     }
 }
